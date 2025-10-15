9f765c6faf01ca7cf71f8e989872ddb3d25cc46b
Slimane <slimanbouaroua900@gamil.com>
2025-10-15 22:16:52 +0200

    Creation la class AjouterItem qui ajoute une entree

---
+++ src/main/java/fr/uvsq/cprog/collex/AjouterItem.java
@@ -0,0 +1,28 @@
+package fr.uvsq.cprog.collex;
+
+import java.io.IOException;
+
+public class AjouterItem implements Commande{
+
+    private Dns dns;
+    private AdresseIP ip;
+    private NomMachine nom;
+
+    public AjouterItem(Dns dns, AdresseIP ip, NomMachine nom) {
+        this.dns = dns;
+        this.ip = ip;
+        this.nom = nom;
+    }
+
+    @Override
+    public void execute() {
+        try {
+            dns.addItem(ip, nom);
+            System.out.println("Machine ajoutée avec succès !");
+        } catch (IOException e) {
+            System.out.println("ERREUR : Impossible d’écrire dans le fichier !");
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
