9b6e7d71a315552f5fff0a257007c668ee5d7855
Slimane <slimanbouaroua900@gamil.com>
2025-10-15 18:15:29 +0200

    Ajouter fichier dan.txt et config.properties

--- src/main/java/fr/uvsq/cprog/collex/App.java
+++ src/main/java/fr/uvsq/cprog/collex/App.java
@@ -1,13 +1,22 @@
 package fr.uvsq.cprog.collex;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) throws IOException {
+        Path fichierDns = Paths.get("dns.txt");
+        if (!Files.exists(fichierDns)) {
+            Files.createFile(fichierDns);
+        }
+
         System.out.println( "Hello World!" );
     }
 }
